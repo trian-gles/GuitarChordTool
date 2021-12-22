ea4a0201f6816242d058646ebba369b76fc8c8de
trian-gles <69212477+trian-gles@users.noreply.github.com>
2021-12-22 17:25:16 -0500

    replaced CDNs with dependencies

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var express = require('express');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
-
-app.get("/", (res, req) => { req.sendFile(__dirname + "/index.html") })
+app.use('/svguitar', express.static(__dirname + "/node_modules/svguitar/dist"));
+app.use('/public', express.static(__dirname + "/public"));
+app.use('/bootstrap', express.static(__dirname + "/node_modules/bootstrap/dist"));
+app.get("/", (res, req) => { req.sendFile(__dirname + "/chord.html") });
 
 var server = app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + server.address().port);
---
+++ public/ChordChart.js
@@ -0,0 +1,79 @@
+(function () {
+    var initialSettings = {
+        title: 'F# minor',
+        color: '#000000',
+        strings: 6,
+        frets: 4,
+        position: 9,
+        nutSize: 0.65,
+        strokeWidth: 2,
+        style: 'normal',
+    }
+    var initialChord = {
+        // array of [string, fret | 'x' | 0]
+        fingers: [
+            [1, 2],
+            [2, 3],
+            [3, 3],
+            [6, 'x']
+        ],
+
+        // optional: barres for barre chords
+        barres: [
+            { fromString: 5, toString: 1, fret: 1 },
+        ],
+    }
+
+    $('#chord-input').val(JSON.stringify(initialChord, null, 2))
+
+    Object.keys(initialSettings)
+        .forEach(function (name) {
+            $('#chart-config-form [name=' + name + ']').val(initialSettings[name])
+        })
+
+    // initialize chart
+    var chart = new svguitar
+        .SVGuitarChord('#result')
+        .configure(initialSettings)
+        .chord(initialChord)
+
+    function drawChord(chord, settings) {
+        console.log('Drawing chord ', chord, ' with settings', settings)
+
+        try {
+            chart
+                .configure(settings)
+                .chord(chord)
+                .draw()
+        } catch (err) {
+            alert('Failed to create chart: ' + err.message);
+
+            throw err;
+        }
+    }
+
+    $('#chart-config-form,#chord-form').submit(function (e) {
+        e.preventDefault();
+
+        var settings = $('#chart-config-form')
+            .serializeArray()
+            .reduce(function (acc, cur) {
+                acc[cur.name] = isNaN(cur.value) ? cur.value : Number(cur.value);
+
+                return acc;
+            }, {})
+
+        // get the chord
+        try {
+            var chord = JSON.parse($('#chord-input').val())
+        } catch (err) {
+            alert('Failed to parse the chord. Are you sure you entered valid JSON?');
+
+            return;
+        }
+
+        drawChord(chord, settings);
+    })
+    console.log("Drawing chord");
+    drawChord(initialChord, initialSettings);
+})();
